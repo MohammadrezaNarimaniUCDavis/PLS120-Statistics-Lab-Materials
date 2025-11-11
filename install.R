abd7c4ed8ef0cf405dbd10d5ada3ce57a2f6b627
Mohammadreza_Narimani <mnarimani@ucdavis.edu>
2025-11-11 13:48:00 -0800

    Complete Week 7: Functions and T-Tests materials

--- install.R
+++ install.R
@@ -1,2 +1,12 @@
-install.packages(c("ggplot2", "dplyr", "readr", "IRkernel"))
+# Install required R packages for PLS 120 Week 7
+# Functions and T-Tests
+
+install.packages(c(
+  "ggplot2",    # Data visualization
+  "dplyr",      # Data manipulation
+  "readr",      # Reading CSV files
+  "IRkernel"    # Jupyter R kernel
+))
+
+# Install Jupyter R kernel
 IRkernel::installspec()
